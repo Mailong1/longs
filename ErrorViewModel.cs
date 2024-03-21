527a90924ef6a20353288d39305fde2dd6684885
Mailong1 <89790935+Mailong1@users.noreply.github.com>
2024-03-21 20:28:46 +0700

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SV20T1020451.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PerSon.cs
@@ -0,0 +1,11 @@
+﻿namespace SV20T1020451.Web.Models
+{
+    public class PerSon
+    {
+        public String Name { get; set; } = "";
+        public DateTime BirthDate {  get; set; }
+        public decimal Salary { get; set; }
+
+
+    }
+}
